515b3f2f679b55effc0c21b89348649524297336
Kinmaro212 <toti98741@gmail.com>
2024-11-19 23:09:42 +0100

    modication jeux

---
+++ script.js
@@ -0,0 +1,9 @@
+const box = document.createElement("div")
+box.classList.add("box")
+
+const board = document.querySelector("#boaard")
+
+function shuffleChildren(parent){
+    let children = parent.childrenlet i  =  children.lenght, k , temp
+    while()
+}
